11144af72eeb6ba1d016c1ede574f2234da6aeef
pheaem <pheaem@puc.edu.kh>
2024-01-25 16:17:15 +0700

    env

--- core_project/settings.py
+++ core_project/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2e16d)t#=hghp5v1rbw9*&77ctgmu9p83ijk0k1*nw)lwwk%jf'
+SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-2e16d)t#=hghp5v1rbw9*&77ctgmu9p83ijk0k1*nw)lwwk%jf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if ENVIRONMENT == 'development':
